b8382f7658f6edca855031ca79433646be4d7b02
binitamuskan996 <binitabini05@gmail.com>
2025-12-02 16:33:05 +0530

    add req.method and req.url code

--- ecomm.js
+++ ecomm.js
@@ -2,30 +2,33 @@ const express = require('express');
 const app = express();
 const port = 4000;
 
-// Middleware to parse JSON data in request bodies
-app.use(express.json());
+// Logging Middleware
+app.use((req, res, next) => {
+  console.log(`${req.method} request made to ${req.url}`);
+  next(); 
+});
 
 // GET endpoint for /products
 app.get('/products', (req, res) => {
-  console.log('GET /products - Request received');
+  //console.log('GET /products - Request received');
   res.send('Here is the list of all products.');
 });
 
 // POST endpoint for /products
 app.post('/products', (req, res) => {
-  console.log('POST /products - Request received');
+  //console.log('POST /products - Request received');
   res.send('A new product has been created.');
 });
 
 // GET endpoint for /categories
 app.get('/categories', (req, res) => {
-  console.log('GET /categories - Request received');
+  //console.log('GET /categories - Request received');
   res.send('Here is the list of all categories.');
 });
 
 // POST endpoint for /categories
 app.post('/categories', (req, res) => {
-  console.log('POST /categories - Request received');
+  //console.log('POST /categories - Request received');
   res.send('A new category has been added.');
 });
 
