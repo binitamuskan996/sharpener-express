eb123c72238e521b6e4a9bfba5bb1e9cedc8f6cf
binitamuskan996 <binitabini05@gmail.com>
2025-12-03 16:15:45 +0530

    add routes

--- order.js
+++ order.js
@@ -1,33 +1,37 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const orderRoutes=require('./routes/orders')
+const userRoutes=require('./routes/users')
 
-// Middleware to parse JSON data in request bodies
-app.use(express.json());
+app.use('/orders',orderRoutes);
+app.use('/users',userRoutes)
+// // Middleware to parse JSON data in request bodies
+// app.use(express.json());
 
-// GET endpoint for /orders
-app.get('/orders', (req, res) => {
-  console.log('GET /orders - Request received');
-  res.send('Here is the list of all orders.');
-});
+// // GET endpoint for /orders
+// app.get('/orders', (req, res) => {
+//   console.log('GET /orders - Request received');
+//   res.send('Here is the list of all orders.');
+// });
 
-// POST endpoint for /orders
-app.post('/orders', (req, res) => {
-  console.log('POST /orders - Request received');
-  res.send('A new order has been created.');
-});
+// // POST endpoint for /orders
+// app.post('/orders', (req, res) => {
+//   console.log('POST /orders - Request received');
+//   res.send('A new order has been created.');
+// });
 
-// GET endpoint for /users
-app.get('/users', (req, res) => {
-  console.log('GET /users - Request received');
-  res.send('Here is the list of all users.');
-});
+// // GET endpoint for /users
+// app.get('/users', (req, res) => {
+//   console.log('GET /users - Request received');
+//   res.send('Here is the list of all users.');
+// });
 
-// POST endpoint for /users
-app.post('/users', (req, res) => {
-  console.log('POST /users - Request received');
-  res.send('A new user has been added.');
-});
+// // POST endpoint for /users
+// app.post('/users', (req, res) => {
+//   console.log('POST /users - Request received');
+//   res.send('A new user has been added.');
+// });
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
